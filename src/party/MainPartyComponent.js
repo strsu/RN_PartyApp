a71d664de8f8a04d14ded08c3e2d0e4ca49e194f
park young jae <jolie0215@gmail.com>
2022-05-16 00:00:07 +0900

    mainparty_filter
    codeup part of mainparty filtering

--- src/party/MainPartyComponent.js
+++ src/party/MainPartyComponent.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import {
+    Animated,
+}
+from 'react-native'
 
 import { customAxios } from '../customAxios';
 import useStore from '../../AppContext';
@@ -21,6 +25,37 @@ class MainPartyComponent extends React.Component {
             refreshing: false,
             setRefreshing: this.setRefreshing.bind(this),
             getRefreshing: this.getRefreshing.bind(this),
+
+            filterModalVisiable: false,
+            scheduleModalVisiable: false,
+            setModalVisiable: this.setModalVisiable.bind(this),
+
+            animation: new Animated.Value(0),
+
+            multiSliderValue: [0,100],
+            multiSliderOffsetX: 0,
+            multiSliderOffsetY: 0,
+            setMultiSliderValue: this.setMultiSliderValue.bind(this),
+            setMultiSliderOffsetX: this.setMultiSliderOffsetX.bind(this),
+            setMultiSliderOffsetY: this.setMultiSliderOffsetY.bind(this),
+
+            startDayStyle: {startingDay: true, color: '#50cebb', textColor: 'white'},
+            middleDayStyle: {color: '#70d7c7', textColor: 'white'},
+            endDayStyle: {endingDay: true, color: '#50cebb', textColor: 'white'},
+
+            startDay: '',
+            endDay: '',
+            startStamp: -1,
+            endStamp: -1,
+            markedDates: {},
+            setPeriod: this.setPeriod.bind(this),
+
+            location: ['서울','부산','대구','대전','광주','세종','충북','충남','전북','전남','경북','경남','제주도'],
+
+            scrollEnabled: true,
+            enableScroll: this.enableScroll.bind(this),
+            disableScroll: this.disableScroll.bind(this),
+
         };
 
         
@@ -67,6 +102,103 @@ class MainPartyComponent extends React.Component {
                 state={this.state}
             />
         );
+    }
+
+    setModalVisiable(type, bool) {
+        if(type == 'filter') {
+            this.setState({
+                filterModalVisiable: bool
+            });
+        } else {
+            this.setState({
+                scheduleModalVisiable: bool
+            });
+        }
+    }
+
+    enableScroll = () => this.setState({ scrollEnabled: true });
+    disableScroll = () => this.setState({ scrollEnabled: false });
+    setMultiSliderValue = (value) => this.setState({multiSliderValue: value});
+    setMultiSliderOffsetX = (value) => console.log(value); //this.setState({multiSliderOffsetX: value});
+    setMultiSliderOffsetY = (value) => this.setState({multiSliderOffsetY: value});
+
+    setPeriod(value) {
+        if(this.state.startDay == '' || (this.state.endDay != '' && this.state.startStamp > value.timestamp)) {
+            this.setState({
+                startDay: value.dateString,
+                startStamp: value.timestamp,
+            });
+            if(this.state.endDay != '') {
+                let result = [];
+                let curDate = new Date(value.dateString);
+                while(curDate <= new Date(this.state.endDay)) {
+                    let year = curDate.getFullYear();
+                    let month = String(curDate.getMonth()+1).padStart(2, '0');
+                    let day = String(curDate.getDate()).padStart(2, '0');
+                    result.push(`${year}-${month}-${day}`);
+                    curDate.setDate(curDate.getDate() + 1);
+                }
+                let json = {}
+                for(let i=0; i<result.length; i++) {
+                    if(i == 0) {
+                        json[result[i]] = this.state.startDayStyle;
+                    } else if(i == result.length-1) {
+                        json[result[i]] = this.state.endDayStyle;
+                    } else {
+                        json[result[i]] = this.state.middleDayStyle;
+                    }
+                }
+                this.setState({
+                    markedDates: json,
+                });
+            }
+        } else if(this.state.startDay == value.dateString || this.state.endDay == value.dateString) {
+            this.setState({
+                startDay: '',
+                endDay: '',
+                markedDates: {},
+            });
+        } else {
+            let startDay = this.state.startDay;
+            let endDay = value.dateString;
+
+            if(this.state.startStamp > value.timestamp) {
+                this.setState({
+                    startStamp: value.timestamp,
+                    startDay: value.dateString,
+                    endDay: this.state.startDay,
+                });
+                startDay = value.dateString;
+                endDay = this.state.startDay;
+            }
+
+            console.log(startDay, endDay);
+
+            let result = [];
+            let curDate = new Date(startDay);
+            while(curDate <= new Date(endDay)) {
+                let year = curDate.getFullYear();
+                let month = String(curDate.getMonth()+1).padStart(2, '0');
+                let day = String(curDate.getDate()).padStart(2, '0');
+                result.push(`${year}-${month}-${day}`);
+                curDate.setDate(curDate.getDate() + 1);
+            }
+            let json = {}
+            for(let i=0; i<result.length; i++) {
+                if(i == 0) {
+                    json[result[i]] = this.state.startDayStyle;
+                } else if(i == result.length-1) {
+                    json[result[i]] = this.state.endDayStyle;
+                } else {
+                    json[result[i]] = this.state.middleDayStyle;
+                }
+            }
+
+            this.setState({
+                endDay: value.dateString,
+                markedDates: json,
+            });
+        }
     }
 
     setCurFilter(params) {
--- src/party/MainPartyPresenter.js
+++ src/party/MainPartyPresenter.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
     SafeAreaView,
     FlatList,
@@ -9,14 +9,18 @@ import {
     TouchableOpacity,
     TextInput,
     Dimensions,
-    RefreshControl
+    RefreshControl,
+    Modal,
+    ScrollView,
+    Animated,
+    Pressable
 }
 from 'react-native'
 
 import Icon from 'react-native-vector-icons/Octicons';
-import ModalDropdown from 'react-native-modal-dropdown';
-
 import MainPartyStyle from './MainPartyStyle';
+import MultiSlider from '@ptomasroos/react-native-multi-slider';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -33,7 +37,7 @@ function MainPartyPresenter(props) {
     const onRefresh = React.useCallback(() => {
         props.state.setRefreshing(true);
         wait(2000).then(() => props.state.setRefreshing(false));
-      }, []);
+    }, []);
 
     return (
         <SafeAreaView style={MainPartyStyle.container}>
@@ -45,15 +49,15 @@ function MainPartyPresenter(props) {
                     //extraData={filterValue}
                     onEndReached={props.state.loadMoreOlderData}
                     onEndReachedThreshold={0.7}
-                    ListHeaderComponent={dropDownMenu(props)}
+                    ListHeaderComponent={Header(props)}
                     refreshControl={
                         <RefreshControl
-                          refreshing={props.state.getRefreshing()}
-                          onRefresh={onRefresh}
+                            refreshing={props.state.getRefreshing()}
+                            onRefresh={onRefresh}
                         />
-                      }
+                    }
                     numColumns={2}
-                    renderItem={ item => boardCard( {navigation, item} ) }
+                    renderItem={item => boardCard({ navigation, item })}
                 />
             </View>
         </SafeAreaView>
@@ -64,39 +68,39 @@ export default MainPartyPresenter;
 
 const boardCard = (props) => {
     // 여기서 if로 boardCard 구분 가능
-    
+
     let item = props.item.item;
     const idx = props.item.index;
     //console.log(props.navigation.getParent().getParent().getState());
 
     return (
-            <View style={MainPartyStyle.line} key={idx} >
-                <TouchableOpacity
-                    style={MainPartyStyle.col}
-                    onPress={() => props.navigation.getParent().getParent().navigate('PartyNavigator', {params: item, screen: 'MainPartyDetail'})}
-                >
+        <View style={MainPartyStyle.line} key={idx} >
+            <TouchableOpacity
+                style={MainPartyStyle.col}
+                onPress={() => props.navigation.getParent().getParent().navigate('PartyNavigator', { params: item, screen: 'MainPartyDetail' })}
+            >
 
-                    <View style={MainPartyStyle.lineTop}>
-                        <Image
-                            style={MainPartyStyle.lineTopImg}
-                            source={{uri: item.images == '' ? 'http://192.168.1.243:4001/media/image/?imageName=mainParty 3.jpg' : item.images}}
-                            
-                            //onLoad={ () => {console.log('loaded', item["title"])}}
-                            //onLoadStart={ () => {console.log('loading', item["title"])}}
-                        />
-                    </View>
+                <View style={MainPartyStyle.lineTop}>
+                    <Image
+                        style={MainPartyStyle.lineTopImg}
+                        source={{ uri: item.images == '' ? 'http://192.168.1.243:4001/media/image/?imageName=mainParty 3.jpg' : item.images }}
 
-                    <View style={MainPartyStyle.content}>
-                        <View style={MainPartyStyle.contentTop}>
-                            <View>
-                                <Text
-                                    style={{
-                                        fontSize: 15,
-                                        fontWeight: "700",
-                                    }}
-                                >{item["title"]}</Text>
-                            </View>
-                            {/*<View style={MainPartyStyle.contentTopRight}>
+                    //onLoad={ () => {console.log('loaded', item["title"])}}
+                    //onLoadStart={ () => {console.log('loading', item["title"])}}
+                    />
+                </View>
+
+                <View style={MainPartyStyle.content}>
+                    <View style={MainPartyStyle.contentTop}>
+                        <View>
+                            <Text
+                                style={{
+                                    fontSize: 15,
+                                    fontWeight: "700",
+                                }}
+                            >{item["title"]}</Text>
+                        </View>
+                        {/*<View style={MainPartyStyle.contentTopRight}>
                                 <View style={{
                                     flexDirection: 'row',
                                 }}>
@@ -110,36 +114,252 @@ const boardCard = (props) => {
                                     <Text style={{marginLeft: 5, marginRight: 5,}}>{"2/"+item.ableF}</Text>
                                 </View>
                             </View>*/}
-                            
-                        </View>
 
-                        <View style={MainPartyStyle.contentMid}>
-                            <Icon name="calendar" size={15} color="#BAE7AF" />
-                            <Text
-                                style={{
-                                    marginLeft: 10,
-                                }}
-                            >{item["mdate"]}</Text>
-                        </View>
-
-                        <View style={MainPartyStyle.contentBtm}>
-                            <Icon name="location" size={15} color="#BAE7AF" />
-                            <Text
-                                style={{
-                                    marginLeft: 10,
-                                }}
-                            >{item["place"]}</Text>
-                        </View>
                     </View>
-                    
-                    
-                </TouchableOpacity>
-            </View>
-        
+
+                    <View style={MainPartyStyle.contentMid}>
+                        <Icon name="calendar" size={15} color="#BAE7AF" />
+                        <Text
+                            style={{
+                                marginLeft: 10,
+                            }}
+                        >{item["mdate"]}</Text>
+                    </View>
+
+                    <View style={MainPartyStyle.contentBtm}>
+                        <Icon name="location" size={15} color="#BAE7AF" />
+                        <Text
+                            style={{
+                                marginLeft: 10,
+                            }}
+                        >{item["place"]}</Text>
+                    </View>
+                </View>
+
+
+            </TouchableOpacity>
+        </View>
+
     );
 };
 
-const dropDownMenu = (props) => {
+const Header = (props) => {
+    return (
+        <View style={{
+            flexDirection: 'row',
+        }}>
+            <TouchableOpacity
+                onPress={() => props.state.setModalVisiable('filter', true)}
+            >
+                {
+                    props.state.filterModalVisiable ?
+                        <Filter state={props.state} /> :
+                        <></>
+                }
+                <Icon name="filter" size={30} color="black" />
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                onPress={() => props.state.setModalVisiable('schedule', true)}
+            >
+                {
+                    props.state.scheduleModalVisiable ?
+                        <Schedule state={props.state} /> :
+                        <></>
+                }
+                <Icon name="calendar" size={30} color="black" />
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const Filter = ({ state }) => {
+
+    const ageAnimation = {
+        transform: [
+            {scale: state.animation},
+            {
+                translateY: state.animation.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [0, -160]
+                })
+            }
+        ]
+    };
+
+    return (
+        <Modal
+            animationType='fade'
+            transparent={true}
+            visible={state.filterModalVisiable}
+        >
+            <View style={{
+                flex: 1,
+                marginTop: 140,
+                marginBottom: 140,
+                marginLeft: 60,
+                marginRight: 60,
+                backgroundColor: 'white',
+            }}>
+
+                <ScrollView
+                    style={{ flex: 1, padding: 10, }}
+                    scrollEnabled={state.scrollEnabled}>
+
+                    <View>
+                        <TouchableOpacity style={{
+                            padding: 5,
+                            backgroundColor: 30,
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                        }}
+                            onPress={() => {
+                                Animated.spring(state.animation, {
+                                    toValue: 1,
+                                    friction: 5,
+                                    useNativeDriver: false
+                                }).start();
+                            }}
+                        >
+                            <Text>나이</Text>
+                            <Icon name='triangle-down' size={25} />
+                        </TouchableOpacity>
+                        
+                            <Animated.View style={[{
+                                justifyContent: 'center',
+                                alignItems: 'center',
+
+                            }, ageAnimation]}>
+
+                                <MultiSlider
+                                    sliderLength={parseInt(windowWidth / 1.8)}
+                                    isMarkersSeparated={true}
+                                    values={[state.multiSliderValue[0], state.multiSliderValue[1]]}
+                                    min={0}
+                                    max={100}
+                                    allowOverlap={false}
+                                    minMarkerOverlapDistance={10}
+                                    touchDimensions={{
+                                        height: 40,
+                                        width: 40,
+                                        borderRadius: 20,
+                                        slipDisplacement: 40
+                                    }}
+                                    onValuesChange={state.setMultiSliderValue}
+                                    onValuesChangeStart={state.disableScroll}
+                                    onValuesChangeFinish={state.enableScroll}
+
+                                    customMarkerLeft={(e) => {
+                                        return (
+                                            <View>
+                                                <View style={{
+                                                    position: 'absolute',
+                                                    top: -5,
+                                                    width: 10,
+                                                    height: 10,
+                                                    borderRadius: 100,
+                                                    backgroundColor: 'green',
+                                                }}></View>
+                                                <Text style={{
+                                                    position: 'absolute',
+                                                    top: -25,
+                                                }}>{e.currentValue}</Text>
+                                            </View>
+                                        )
+                                    }}
+
+                                    customMarkerRight={(e) => {
+                                        return (
+                                            <View>
+                                                <View style={{
+                                                    position: 'absolute',
+                                                    top: -5,
+                                                    width: 10,
+                                                    height: 10,
+                                                    borderRadius: 100,
+                                                    backgroundColor: 'green',
+                                                }}></View>
+                                                <Text style={{
+                                                    position: 'absolute',
+                                                    top: -25,
+                                                }}>{e.currentValue}</Text>
+                                            </View>
+                                        )
+                                    }}
+                                />
+                            </Animated.View>
+                    </View>
+
+
+
+                    <View>
+                        <Text>일정</Text>
+                    </View>
+
+                    <View>
+                        <Calendar
+                            onDayPress={day => {
+                                console.log('selected day', day);
+                                state.setPeriod(day);
+                            }}
+                            enableSwipeMonths={true}
+                            markingType={'period'}
+                            markedDates={state.markedDates}
+                        //displayLoadingIndicator={true}
+                        />
+                    </View>
+
+                    <View>
+                        <Text>지역</Text>
+                    </View>
+
+                    <View style={{
+                        flexDirection: 'row',
+                        flexWrap: 'wrap',
+                        marginBottom: 20,
+                    }}>
+                        {
+                            state.location.map(data => {
+                                return (
+                                    <TouchableOpacity style={{
+                                        padding: 5,
+                                        paddingLeft: 10,
+                                        paddingRight: 10,
+                                        backgroundColor: 40,
+                                        margin: 3,
+                                        borderRadius: 20,
+                                    }}>
+                                        <Text style={{
+                                        }}>{data}</Text>
+                                    </TouchableOpacity>
+                                );
+
+                            })
+                        }
+                    </View>
+
+                </ScrollView>
+
+            </View>
+        </Modal>
+    );
+}
+
+
+const Schedule = ({ state }) => {
+    return (
+        <Modal
+            animationType='fade'
+            transparent={true}
+            visible={state.scheduleModalVisiable}
+        >
+
+        </Modal>
+    );
+}
+
+/*const dropDownMenu = (props) => {
     return(
         <ModalDropdown 
             options={props.state.filterData}
@@ -192,5 +412,5 @@ const dropDownMenu = (props) => {
             </View>
         </ModalDropdown>
     );
-}
+}*/
 
